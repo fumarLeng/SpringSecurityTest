61e3f2b3f504560a2e13b938430137ed1bafb21f
Leng <no@gmail.com>
2024-05-22 10:13:22 +0800

    feat: SpringSecurity練習

---
+++ src/main/java/com/example/sringsecurity2test/SringSecurity2TestApplication.java
@@ -0,0 +1,13 @@
+package com.example.sringsecurity2test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SringSecurity2TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SringSecurity2TestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sringsecurity2test/SringSecurity2TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sringsecurity2test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SringSecurity2TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
